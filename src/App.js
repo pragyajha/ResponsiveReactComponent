e2b887b77b5861c21e4466617e9c9b9a7eaaad6f
pragyajha <pragya.jha@treebohotels.com>
2019-12-01 12:57:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveResultItem from '../src/views/ResponsiveResultItem';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/utils/theme';
 
 function App() {
+  console.log({theme});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <ResponsiveResultItem />
+      </ThemeProvider>
   );
 }
 
