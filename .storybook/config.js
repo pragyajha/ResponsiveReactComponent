6144907056140cebca2b0308e9458bec8d81445e
pragyajha <pragya.jha@treebohotels.com>
2019-12-01 23:32:14 +0530

    add viewport configuration to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,30 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+
+
+const newViewports = {
+  mobile: {
+    name: 'Mobile',
+    styles: {
+      width: '500px',
+      height: '963px',
+    },
+  },
+  desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1024px',
+      height: '1980px',
+    },
+  },
+};
+ 
+addParameters({
+    viewport: {
+      viewports: {
+        ...newViewports,
+      },
+    },
+  });
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
--- stories/ResponsiveResultItem.stories.js
+++ stories/ResponsiveResultItem.stories.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import App from '../src/App';
+import { storiesOf, addDecorator } from '@storybook/react';
+import { withViewport } from '@storybook/addon-viewport';
 
+storiesOf('ResponsiveBoatListItem', module)
+.add('desktop', () => (
+  <App/>
+), { viewport: { defaultViewport: 'desktop' }});
 
-export default {
-  title: 'ResponsiveBoatListItem',
-};
-
-export const toStorybook = () => <App/>;
-
-toStorybook.story = {
-  name: 'view',
-};
+storiesOf('ResponsiveBoatListItem', module)
+.add('mobile', () => (
+  <App/>
+), { viewport: { defaultViewport: 'mobile' }});
